de845527d4e8bbfb3bb5b9b23ee423568713d5ad
scott <scttsmrfng@gmail.com>
2026-05-10 16:20:55 +0800

    JenkinsCI

--- src/test/java/testing/Testing.java
+++ src/test/java/testing/Testing.java
@@ -21,70 +21,70 @@ public class Testing extends BasePage {
 
 
 
-//	@Test (dataProvider = "getData", groups = {"Smoke"}, retryAnalyzer=Retry.class)
-//	public void TC2(String uname, String pword) {
-//
-//		landingpage.openWebsite("https://github.com");
-//		
-//		SignUpPage spage = landingpage.verifyClickedHeader();
-//		spage.signUpUser(uname, pword);
-//		landingpage.explicitWait("//input [@id = 'password']");
-//		
-////		Assert.fail();
-//
-//
-//	}
-//	
-//
-//	@Test (dataProvider = "getData", dependsOnMethods = "TC2")
-//	public void TC1(String uname, String pword) {
-//
-//		landingpage.openWebsite("https://github.com");
-//		SignUpPage spage = landingpage.verifyClickedHeader();
-//		spage.signUpUser(uname, pword);
-//
-//
-//	}
-//	
-//	
-//	@Test
-//	public void actionsTest() {
-//		landingpage.openWebsite("https://github.com");
-//		landingpage.mouseHover("//div/button[normalize-space() = 'Solutions']");
-//		landingpage.inputUpperCase("//span[normalize-space() = 'Search or jump to...']");
-//		
-//		
-//	}
-//	
-//	@Test
-//	public void windowTabTest() {
-//		
-//		
-//		landingpage.openWebsite("https://rahulshettyacademy.com/loginpagePractise/#");
-//		landingpage.windowHandle();
-//		
-//		
-//		
-//	}
-//	
-//	@Test
-//	public void FrameHandle() {
-//		
-//		landingpage.openWebsite("https://jqueryui.com");
-//		landingpage.frameHandle();
-//		
-//		
-//	}
-//	
-//	@Test (dataProvider = "getCountryData")
-//	public void testDropDown(String keyword, String wholeCountry) {
-//		
-//		landingpage.openWebsite("https://rahulshettyacademy.com/dropdownsPractise/");
-//		landingpage.staticDropDown();
-//		landingpage.loopDropdown();
-//		landingpage.autoSuggestiveDropdown(keyword, wholeCountry);
-//		
-//	}
+	@Test (dataProvider = "getData", groups = {"Smoke"}, retryAnalyzer=Retry.class)
+	public void TC2(String uname, String pword) {
+
+		landingpage.openWebsite("https://github.com");
+		
+		SignUpPage spage = landingpage.verifyClickedHeader();
+		spage.signUpUser(uname, pword);
+		landingpage.explicitWait("//input [@id = 'password']");
+		
+//		Assert.fail();
+
+
+	}
+	
+
+	@Test (dataProvider = "getData", dependsOnMethods = "TC2")
+	public void TC1(String uname, String pword) {
+
+		landingpage.openWebsite("https://github.com");
+		SignUpPage spage = landingpage.verifyClickedHeader();
+		spage.signUpUser(uname, pword);
+
+
+	}
+	
+	
+	@Test
+	public void actionsTest() {
+		landingpage.openWebsite("https://github.com");
+		landingpage.mouseHover("//div/button[normalize-space() = 'Solutions']");
+		landingpage.inputUpperCase("//span[normalize-space() = 'Search or jump to...']");
+		
+		
+	}
+	
+	@Test
+	public void windowTabTest() {
+		
+		
+		landingpage.openWebsite("https://rahulshettyacademy.com/loginpagePractise/#");
+		landingpage.windowHandle();
+		
+		
+		
+	}
+	
+	@Test (groups = {"Sanity"})
+	public void FrameHandle() {
+		
+		landingpage.openWebsite("https://jqueryui.com");
+		landingpage.frameHandle();
+		
+		
+	}
+	
+	@Test (dataProvider = "getCountryData", groups = {"Regression"})
+	public void testDropDown(String keyword, String wholeCountry) {
+		
+		landingpage.openWebsite("https://rahulshettyacademy.com/dropdownsPractise/");
+		landingpage.staticDropDown();
+		landingpage.loopDropdown();
+		landingpage.autoSuggestiveDropdown(keyword, wholeCountry);
+		
+	}
 	
 	@Test
 	public void testCalendar() {
